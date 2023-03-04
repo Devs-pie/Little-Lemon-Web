fd870bcf2c1ed0e2cad087e5b94cc807377bf23f
Div_pie <jophyel.fronted@gmail.com>
2023-03-03 23:04:10 -0600

    adding the App.js code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.scss";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+// Routes
+import { Routes, Route } from "react-router-dom";
+import BookingWithSteps from "./components/BookingWithSteps";
+import Page404 from "./components/Page404";
+import PageHome from "./components/PageHome";
+import PageAbout from "./components/PageAbout";
+import PageOrder from "./components/PageOrder";
+import PageBooking from "./components/PageBooking";
+import PageLogin from "./components/PageLogin";
 
+// Global Settings
+const siteName = "Little Lemon Chicago";
+const mainMenu = {
+  Home: "/",
+  About: "/about",
+  Booking: "/booking",
+  "Order online": "/order",
+  Login: "/login",
+};
+const multiStepsBooking = false;
+
+// App
 function App() {
+  const updatedMainMenu = multiStepsBooking
+    ? { ...mainMenu, Booking: "/booking-steps" }
+    : { ...mainMenu };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/Apph.jsd</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header mainMenu={updatedMainMenu} />
+      <main>
+        <Routes>
+          <Route path="*" element={<Page404 />} />
+          <Route path="/" element={<PageHome links={updatedMainMenu} />} />
+          <Route path="/about" element={<PageAbout />} />
+          <Route path="/order" element={<PageOrder />} />
+          {multiStepsBooking ? (
+            <Route path="/booking-steps" element={<BookingWithSteps />} />
+          ) : (
+            <Route path="/booking" element={<PageBooking />} />
+          )}
+          <Route path="/login" element={<PageLogin />} />
+        </Routes>
+      </main>
+      <Footer mainMenu={updatedMainMenu} siteName={siteName} />
+    </>
   );
 }
 
 export default App;
+
